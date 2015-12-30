d572f8ed5c2f9ab760f807bfcbdef3f3cb5765d5
philip <deng4zhi4jie2@gmail.com>
2015-12-30 20:18:26 +0800

    Add user controller

--- lumen/app/Http/Controllers/UserController.php
+++ lumen/app/Http/Controllers/UserController.php
@@ -9,21 +9,88 @@
 namespace App\Http\Controllers;
 
 
+use App\Helpers\CommonHelper;
+use App\Helpers\FaultCode;
+use App\Helpers\Response;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController
 {
 
+    /**
+     * 注册
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function register(Request $request)
     {
+        // input
         $input = $request->all();
-        return response()->json($input);
+
+        // validate
+        $validator = Validator::make($input, [
+            'mobile' => 'required|size:11',
+            'password' => 'required|min:6',
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            return Response::error(FaultCode::PARAMS_ERROR, $errors);
+        }
+
+        // duplicate
+        $user = User::where('mobile', $input['mobile'])->first();
+        if ($user) {
+            return Response::error(FaultCode::USER_EXIST);
+        }
+
+        // new
+        $user = new User();
+        $user->mobile = $input['mobile'];
+        $user->salt = CommonHelper::getSalt();
+        $user->password = CommonHelper::makePassword($input['password'], $user->salt);
+        $user->save();
+
+        return Response::result([
+            'ok' => true
+        ]);
     }
 
+    /**
+     * 登录
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function login(Request $request)
     {
+        // input
         $input = $request->all();
-        return response()->json($input);
+
+        // validate
+        $validator = Validator::make($input, [
+            'mobile' => 'required|size:11',
+            'password' => 'required|min:6',
+        ]);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            return Response::error(FaultCode::PARAMS_ERROR, $errors);
+        }
+
+        // gulp
+        $user = User::where('mobile', $input['mobile'])->first();
+        if (is_null($user)) {
+            return Response::error(FaultCode::USER_NOT_EXIST);
+        }
+
+        // password
+        if (!CommonHelper::comparePassword($input['password'], $user->password, $user->salt)) {
+            return Response::error(FaultCode::PASSWORD_NOT_MATCH);
+        }
+
+        return Response::result([
+            'ok' => true
+        ]);
     }
 
 }
